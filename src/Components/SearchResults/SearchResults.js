7014e5b9e3b472836f0fb3f0194000a07d5a033f
Juliana Grutzmann <juliana_grutzmann@hotmail.com>
2020-06-26 12:35:20 +1000

    First commit

---
+++ src/Components/SearchResults/SearchResults.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import './SearchResults.css';
+
+import TrackList from '../TrackList/TrackList';
+
+const SearchResults = () => {
+  return (
+    <div className="SearchResults">
+      <h2>Results</h2>
+      <TrackList />
+    </div>
+  )
+}
+
+export default SearchResults;
---
+++ src/Components/Track/Track.js
@@ -0,0 +1,27 @@
+import React from 'react';
+import './Track.css';
+
+let isRemoval;
+
+function renderAction() {
+    return isRemoval ? <button className="Track-action">+</button> 
+    : <button className="Track-action">-</button>
+}
+
+
+
+const Track = () => {
+
+  return (
+    <div className="Track">
+      <div className="Track-information">
+        {/* <h3><!-- track name will go here --></h3> */}
+        {/* <p><!-- track artist will go here--> | <!-- track album will go here --></p> */}
+      </div>
+      {/* <button className="Track-action"><!-- + or - will go here --></button> */}
+    </div>
+  )
+}
+
+export default Track;
+
